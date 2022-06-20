9a2ea57805383d6920f3dc38f6179ef2d90308df
kumab2221 <kumab2221@gmail.com>
2022-06-20 23:08:38 +0900

    init センサーサンプル
    Gセンサー

---
+++ app/src/main/java/com/example/mysensor/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.mysensor
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var mSensor: Sensor? = null
+    private var mHelloWorld : TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        mHelloWorld = findViewById(R.id.hello_world)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        val lux = event.values[0]
+        // Do something with this sensor value.
+        mHelloWorld?.text = lux.toString()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensor?.also { light ->
+            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+}
